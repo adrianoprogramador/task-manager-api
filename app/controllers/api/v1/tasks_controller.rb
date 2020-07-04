6ce9acefa5a6a52c4f38aaccaa504dff0d53b18f
Adriano Carvalho <adrianocarvalhowebmail@gmail.com>
2020-07-03 22:41:33 -0300

    Add action update to the tasks controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -21,11 +21,20 @@ class Api::V1::TasksController < ApplicationController
     end
   end
 
+  def update
+    task = current_user.tasks.find(params[:id])
+
+    if task.update_attributes(task_params)
+      render json: task, status: 200
+    else
+      render json: { errors: task.errors }, status: 422
+    end
+  end
+  
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :deadeline, :done)
-    
+    params.require(:task).permit(:title, :description, :deadeline, :done)    
   end
   
 
